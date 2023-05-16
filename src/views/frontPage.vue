c9a7382c1f44bb9847d86d145083e3cde446047c
Skydem <tomek.torbus@gmail.com>
2023-05-16 19:21:38 +0200

    added fox generator

--- src/views/frontPage.vue
+++ src/views/frontPage.vue
@@ -10,24 +10,21 @@
           </v-row>
         </v-sheet>
         <v-sheet class="section" v-scroll-snap="{ mandatory: true }">
-          <!-- <v-row align="center" justify="center" class="card-group">
-              <v-col cols="12" md="4">
-                <h1>SEKCJA 2</h1>
-              </v-col>
-            </v-row> -->
           <v-row align="center" justify="center">
-            <v-col cols="12" md="4">
+            <v-col cols="12" md="4" v-for="(card, index) in cards" :key="index">
               <v-card>
+                <v-img height="300" :src="card.img" cover></v-img>
                 <v-card-title primary-title>
                   <div>
-                    <h3 class="headline mb-0">Karta 1</h3>
+                    <h3 class="headline mb-0">{{ card.title }}</h3>
                   </div>
                 </v-card-title>
                 <v-card-text>
-                  <p>Treść karty 1.</p>
+                  <p>{{ card.content }}</p>
                 </v-card-text>
                 <v-card-actions>
-                  <v-btn> CLICK ME PLEASE! </v-btn>
+                  <v-btn variant="flat" color="info"> Select fox! </v-btn>
+                  <v-btn @click="test(card)"> Roll the fox. </v-btn>
                 </v-card-actions>
               </v-card>
             </v-col>
@@ -46,12 +43,48 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "App",
   data() {
     return {
-      test: "kappa",
+      cards: [
+        {
+          title: "title1",
+          content: "simple content",
+          img: "https://randomfox.ca/images/4.jpg",
+        },
+        {
+          title: "title2",
+          content: "simple content",
+          img: "https://randomfox.ca/images/21.jpg",
+        },
+        {
+          title: "title3",
+          content: "simple content",
+          img: "https://randomfox.ca/images/37.jpg",
+        },
+        // {
+        //   title: "title4",
+        //   content: "simple content",
+        // },
+      ],
     };
+  },
+  methods: {
+    async getPic() {
+      const getResponse = await axios.get("https://randomfox.ca/floof/");
+      const newPic = getResponse.data.image;
+      console.log("Got new floof", newPic);
+      return newPic;
+    },
+    async test(key) {
+      console.log(`Got card! ${key}`);
+      const newPic = await this.getPic();
+      console.log("New pic!", newPic);
+      key.img = newPic;
+    },
   },
 };
 </script>
