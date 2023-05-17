c43bbb65f998304baa9593a262ec19956644c694
Skydem <tomek.torbus@gmail.com>
2023-05-17 21:15:35 +0200

    template cleanup

---
+++ src/components/FrontPage/SectionSheet.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-sheet
+    class="section"
+    :class="[`bg-grey-lighten-${lighten}`, `px-${px}`]"
+    v-scroll-snap="{ mandatory: true }"
+  >
+    <slot />
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: "SectionSheet",
+  props: {
+    lighten: {
+      type: Number,
+      required: true,
+    },
+    px: {
+      type: Number,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.section {
+  height: 100vh;
+  scroll-snap-align: start;
+}
+</style>
--- src/views/FrontPage.vue
+++ src/views/FrontPage.vue
@@ -2,50 +2,9 @@
   <v-app>
     <v-main>
       <v-container fluid class="px-0">
-        <v-sheet
-          class="section bg-grey-lighten-4 px-14"
-          v-scroll-snap="{ mandatory: true }"
-        >
-          <v-row align="center" justify="center">
-            <v-col cols="12">
-              <h1 class="title">Tomasz Torbus Technical Task</h1>
-            </v-col>
-          </v-row>
-        </v-sheet>
-        <v-sheet
-          class="section bg-grey-lighten-3 px-4"
-          v-scroll-snap="{ mandatory: true }"
-        >
-          <v-row align="center" justify="center">
-            <v-col cols="12" md="4" v-for="(card, index) in cards" :key="index">
-              <FoxCard isFrontPage="True" :singleFox="card">
-                <template #image>
-                  <v-img height="60vh" :src="card.img" cover></v-img>
-                </template>
-                <template #title>
-                  {{ card.title }}
-                </template>
-                <template #attack>
-                  {{ card.stats.attack }}
-                </template>
-                <template #health>
-                  {{ card.stats.health }}
-                </template>
-              </FoxCard>
-            </v-col>
-          </v-row>
-        </v-sheet>
-        <v-sheet
-          class="section bg-grey-lighten-2 px-4"
-          v-scroll-snap="{ mandatory: true }"
-          v-colo
-        >
-          <v-row align="center" justify="center">
-            <v-col cols="12">
-              <h1 class="title">SEKCJA 3</h1>
-            </v-col>
-          </v-row>
-        </v-sheet>
+        <IntroductionSheet />
+        <MainSheet />
+        <FooterSheet />
       </v-container>
     </v-main>
   </v-app>
@@ -53,13 +12,14 @@
 
 <script>
 import router from "@/router";
-import { mapActions, mapMutations } from "vuex";
-import CardStubService from "@/services/CardStubService";
-import FoxCard from "@/components/FoxCard.vue";
+import {mapActions, mapMutations} from "vuex";
+import IntroductionSheet from "@/components/FrontPage/IntroductionSheet.vue";
+import MainSheet from "@/components/FrontPage/MainSheet.vue";
+import FooterSheet from "@/components/FrontPage/FooterSheet.vue";
 
 export default {
-  name: "FrontP0age",
-  components: { FoxCard },
+  name: "FrontPage",
+  components: {FooterSheet, MainSheet, IntroductionSheet},
   data: () => ({
     cards: [],
   }),
@@ -67,12 +27,7 @@ export default {
     ...mapMutations({
       saveStoreFox: "saveFox",
     }),
-    ...mapActions(["getPic", "getName", "rollFox"]),
-    async refreshAll() {
-      this.cards.forEach((fox) => {
-        this.rollFox(fox);
-      });
-    },
+    ...mapActions(['getPic', 'getName']),
     saveFox(key) {
       this.saveStoreFox(key);
       router.push({ name: "fightPage" });
@@ -93,16 +48,5 @@ body {
   margin: 0;
   scroll-snap-type: y mandatory;
   overflow-y: scroll;
-}
-
-.section {
-  height: 100vh;
-  scroll-snap-align: start;
-}
-.title {
-  font-size: 32px;
-  text-align: center;
-  margin-top: 50vh;
-  transform: translateY(-50%);
 }
 </style>
