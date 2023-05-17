a8c46f307b4d71b48dc4d16e3b9631cebe0127f0
Skydem <tomek.torbus@gmail.com>
2023-05-17 19:42:23 +0200

    store cleanup

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,13 @@
 import { createStore } from "vuex";
 
+const actions = {
+  saveFox({ commit }, foxArray) {
+    console.log("Got fox?", foxArray);
+    console.log(foxArray);
+    this.state.fox = foxArray;
+  },
+};
+
 export default createStore({
   state: {
     fox: {
@@ -11,13 +19,7 @@ export default createStore({
       },
     },
   },
-  actions: {
-    saveFox({ commit }, foxArray) {
-      console.log("Got fox?", foxArray);
-      console.log(foxArray);
-      this.state.fox = foxArray;
-    },
-  },
+  actions,
   getters: {
     choosenFox: (state) => {
       return state.fox;
